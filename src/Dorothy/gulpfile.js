9cd889b34d49254186d1c8663f97f8636b3f1b03
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2016-03-10 21:03:50 +0100

    Match design to invitations.

--- src/Dorothy/gulpfile.js
+++ src/Dorothy/gulpfile.js
@@ -1,55 +1,41 @@
-﻿/// <binding Clean='clean' />
-
-var project = require("./project.json");
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var minifyJs = require('gulp-uglify');
-var rimraf = require('rimraf');
+﻿var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var cssnano = require("gulp-cssnano");
+var rename = require("gulp-rename");
+var minifyJs = require("gulp-uglify");
+var rimraf = require("rimraf");
+var prefixer = require("gulp-autoprefixer");
 
 
 var paths = {
-    webroot: "./" + project.webroot + "/"
+    js: ["wwwroot/js/**/*.js", "!wwwroot/js/**/*.min.js"],
+    css: ["wwwroot/css/**/*.scss"]
 };
-paths.js = paths.webroot + "js/**/*.js";
-paths.jsDest = paths.webroot + "js/site.min.js";
-paths.css = paths.webroot + "css/**/*.scss";
-paths.cssDest = paths.webroot + "css/site.min.css";
 
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.jsDest, cb);
-});
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.cssDest, cb);
-});
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-
-gulp.task('js', ['clean:js'], function () {
-    gulp.src([paths.js, '!*.min.js'], { base: '.' })
-      .pipe(concat('site.js'))
-      .pipe(minifyJs())
-      .pipe(rename(paths.jsDest))
-      .pipe(gulp.dest('.'));
+gulp.task("js", function () {
+    return gulp.src(paths.js, { base: "." })
+        .pipe(concat("site.js"))
+        .pipe(rename("wwwroot/js/site.min.js"))
+        .pipe(gulp.dest("."));
 });
 
 
-gulp.task('css', ['clean:css'], function () {
-    gulp.src(paths.css, { base: '.' })
-      .pipe(sass())
-      .pipe(concat('site.css'))
-      .pipe(minifyCss())
-      .pipe(rename(paths.cssDest))
-      .pipe(gulp.dest('.'));
+gulp.task("css", function () {
+    return gulp.src(paths.css, { base: "." })
+        .pipe(sass().on("error", sass.logError))
+        .pipe(concat("site.css"))
+        .pipe(prefixer({ browsers: ["last 3 versions"] }))
+        .pipe(cssnano())
+        .pipe(rename("wwwroot/css/site.min.css"))
+        .pipe(gulp.dest("."));
 });
 
 
-gulp.task('watch', function () {
-    gulp.watch([paths.css, '!*.min.css'], ['css']);
-    gulp.watch([paths.js, '!*.min.js'], ['js']);
+gulp.task("watch", function () {
+    gulp.watch(paths.css, ["css"]);
+    gulp.watch(paths.js, ["js"]);
 });
 
-gulp.task('default', ['js', 'css', 'watch']);
+gulp.task("default", ["js", "css", "watch"]);
